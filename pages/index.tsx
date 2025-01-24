959c4a990b0e4c606c67b1d1c5b5c9b04a045796
Parin Mittrarath <parinmittrarath@gmail.com>
2025-01-24 21:23:59 +0700

    version24jan923pm

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,48 @@
+import { datePickerAnatomy } from "@ark-ui/react";
 import {
   Box,
   Heading,
   VStack,
+  Badge,
+  AbsoluteCenter, Center, Circle, Square,
 } from "@chakra-ui/react"
-import Image from "next/image"
-
+import{
+  useState
+} from "react"
+import { TiEdit } from "react-icons/ti";
 export default function Page() {
-  return (
+  const[second,setsec] = useState<number>();
+  const[minute,setmin] = useState<number>();
+  const[hour,sethour] = useState<number>();
+  setTimeout(()=>{
+    const date=new Date();
+    setsec(date.getSeconds());
+    setmin(date.getMinutes());
+    sethour(date.getHours());
+  },1000)
+let hours;
+let minutes;
+let seconds;
+if(hour<10)
+  hours='0'+hour;
+else
+  hours=''+hour;
+if(minute<10)
+   minutes='0'+minute;
+else
+   minutes=''+minute;
+if(second<10)
+   seconds='0'+second;
+else
+   seconds=''+second;
+if(second===undefined){
+  return(
+  <> </>
+  )
+}
+return (
     <>
-      <Box textAlign="center" fontSize="xl" pt="30vh">
-        <VStack gap="8">
-          <Image
-            alt="chakra logo"
-            src="/static/logo.svg"
-            width="80"
-            height="80"
-          />
-          <Heading size="2xl" letterSpacing="tight">
-            Welcome to the template site - Powered by Chakra
-          </Heading>
-
-        </VStack>
-      </Box>
+      <Heading size="6xl"><AbsoluteCenter>{hours} : {minutes} : {seconds}</AbsoluteCenter></Heading>
     </>
   )
 }
