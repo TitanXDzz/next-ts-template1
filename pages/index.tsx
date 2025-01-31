381c6ffed3ddcf188bc20346d851bc1c00496fa0
Parin Mittrarath <parinmittrarath@gmail.com>
2025-01-31 22:06:22 +0700

    31jan20251006pm

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,13 @@
-
+import {
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+  createListCollection,
+  Box,
+} from "@chakra-ui/react"
 import {
   Heading,
   AbsoluteCenter,
@@ -18,7 +27,43 @@ const useClock = ()=>{
   },1000)
   return{second,minute,hour};
 }
-export default function Page() {
+const Choose = () =>{
+  const [clock,setClock]=useState<any>()
+  return(
+    <>
+          <SelectRoot key={"md"} size={"md"} collection={Frameworks} onValueChange={(event)=>{setClock(event.value[0])}}>
+
+            <SelectLabel>CLOCK TYPE???????</SelectLabel>
+            <SelectTrigger>
+              <SelectValueText placeholder="Select type of clock" />
+            </SelectTrigger>
+            <SelectContent>
+              {Frameworks.items.map((movie) => (
+                <SelectItem item={movie} key={movie.value}>
+                  {movie.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </SelectRoot>
+          {clock=="analog"&&<AnalogClock/>}
+          {clock=="digital"&&<Digit/>}
+    </>
+  )
+}
+const Frameworks = createListCollection({
+  items: [
+    { label: "ANALOG", value: "analog" },
+    { label: "DIGITAL", value: "digital" },
+  ],
+})
+export default function Page(){
+  return(
+    <>
+     <Choose></Choose>
+    </>
+  )
+}
+const Digit=() => {
 let hours;
 let minutes;
 let seconds;
@@ -45,4 +90,26 @@ return (
       <Heading size="6xl"><AbsoluteCenter>{hours} : {minutes} : {seconds}</AbsoluteCenter></Heading>
     </>
   )
+}
+const AnalogClock=()=>{
+  return(<>
+    <AbsoluteCenter>
+     <Box
+     width={"500px"}
+     height={"500px"}
+     bgColor={"white"}
+     borderRadius={"250px"}
+     >
+     </Box>
+    </AbsoluteCenter>
+    <AbsoluteCenter>
+    <Box
+     width={"16px"}
+     height={"16px"}
+     bgColor={"black"}
+     borderRadius={"8px"}
+     ></Box>
+    </AbsoluteCenter>
+  
+  </>)
 }
